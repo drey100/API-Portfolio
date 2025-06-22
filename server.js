10c8a47251f58e6af7212ee7d6d2c27098ca1f7e
Tanguy Kakpo <tang.kakpo@gmail.com>
2025-06-22 15:41:29 +0200

    deuxième commit-api

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(bodyParser.json());
 
 // Connexion à MongoDB avec Mongoose
